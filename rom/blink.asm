34773cc42ea4641d7c53fc0f2db174eba9eca2e1
Joshua Bassett <hello@joshbassett.info>
2020-05-05 19:13:06 +1000

    Blink all LEDs in matrix

--- rom/blink.asm
+++ rom/blink.asm
@@ -1,18 +1,47 @@
-  ld sp, $2000 ; set stack pointer address
-  ld hl, $2000 ; set gfx buffer address
+STACK_ADDR: equ $2000 ; stack address
+VRAM_ADDR: equ $2000 ; VRAM address
+NUM_LEDS: equ 64 ; number of LEDs
+DELAY_DURATION: equ $8000; 1 second
 
-loop:
-  ld (hl), $00
-  call delay
-  ld (hl), $ff
-  call delay
-  jp loop
+  di
+  ld sp, STACK_ADDR
 
+start:
+  ld b, NUM_LEDS
+  ld c, $ff
+  ld hl, VRAM_ADDR
+  call fill
+
+  ld bc, DELAY_DURATION
+  call delay
+
+  ld b, NUM_LEDS
+  ld c, $00
+  ld hl, VRAM_ADDR
+  call fill
+
+  ld bc, DELAY_DURATION
+  call delay
+
+  jp start
+
+; Fills a region of memory.
+;
+; b - address count
+; c - fill value
+; hl - start address
+fill:
+  ld (hl), c
+  inc hl
+  djnz fill
+  ret
+
+; Delays for a duration.
+;
+; bc - duration
 delay:
-  ld de, $8000 ; wait for 1s
-_delay:
-  dec de
-  ld a, d
-  or e
-  jp nz, _delay
+  dec bc
+  ld a, b
+  or c
+  jp nz, delay
   ret
